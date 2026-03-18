5a12441d2ab24648783d40b54e6dce767b6628f9
mycampushub <mycampuslifestyle@gmail.com>
2026-03-18 22:29:19 +0600

    fix blank screen base name

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/airline-management/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
